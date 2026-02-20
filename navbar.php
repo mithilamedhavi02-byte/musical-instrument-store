605c4d04640c7b16174adbbc9a006cdbc2dd3e04
Mithila Medhavi <mithilamedhavi02@gmail.com>
2026-02-20 20:20:16 +0530

    update

--- navbar.php
+++
@@ -1,5 +0,0 @@
-<li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
-<li class="nav-item"><a class="nav-link" href="products.php">Shop</a></li>
-<li class="nav-item"><a class="nav-link" href="wishlist.php">Wishlist</a></li>
-<li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
-<li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
